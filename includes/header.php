99ebcf8c7e8d39c30502f1c7f130d52b1a7ca57f
LN <87793296+senocoseno@users.noreply.github.com>
2021-08-18 17:44:22 -0300

    V3.1

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,12 @@
 	include("functions.php");
 /*dontBack();*/
 if(!empty($_GET['error'])) alert($_GET['error']);
+if(isset($_SESSION['ban'])){
+    header("HTTP/1.1 301 Moved Permanently"); 
+    header("Location: https://en.wikipedia.org/wiki/Remember_the_sabbath_day,_to_keep_it_holy"); exit;
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,11 +16,34 @@ if(!empty($_GET['error'])) alert($_GET['error']);
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="http://www.landho.epizy.com/img/favicon.ico?v=2" />
-	<link rel="stylesheet" type="text/css" href="css/style.css?v=3">
     <link rel="preload" href="img/starsbg.gif" as="image">
     <link rel="preload" href="img/favicon.ico" as="image">
     <link rel="preload" href="img/eye.png" as="image">
     <link rel="preload" href="img/eyeslash.png" as="image">
+    <style type="text/css">
+        * { padding: 0px; margin: 0px; font-family: Trattatello Verdana; }
+        body { background-color: black; ; color: white; }
+        textarea { border: 1px solid white; overflow: scroll; max-width:1500px; max-height: 300px; min-width: 250px; min-height: 250px; width:100%; height:100%; outline: none; font-family: Blippo Verdana; letter-spacing: 1px; scrollbar-arrow-color: #000066; scrollbar-base-color: #000033; scrollbar-dark-shadow-color: #336699; scrollbar-track-color: #666633; scrollbar-face-color: #cc9933; scrollbar-shadow-color: #DDDDDD; scrollbar-highlight-color: #CCCCCC; }
+        h1, h2, h3, h4 { text-align: center; }
+
+        a:link { color: white; text-decoration: none; } a:visited { color: white; text-decoration: none; } a:hover { color: gold; text-decoration: none; } a:active { color: white; text-decoration: none; }
+        .header { padding: 30px;  background: #00003F; color: white; font-size: medium; position: relative; }
+        .header a { text-align: left; font-family: papyrus Verdana; }
+        #grad { background-image: linear-gradient(0deg, #00003F, #00008F); }
+
+        .home { display: flex; flex-wrap: wrap; }
+        .in-flex { flex: 1; min-width: 300px; }
+        .in-flex:first-child { flex: 2; margin: 0; }
+        .in-flex div { background: #00003F; padding: 5px; margin: 5px; }
+        #grad2 { background-image: linear-gradient(to right, #00002F, #00005F); }
+        #name::placeholder { color: blue; }
+        .center {vertical-align: center; text-align: center; -webkit-box-sizing:border-box; -moz-box-sizing: border-box; box-sizing: border-box; }
+        input { text-align: center; border: 2px solid #00008F; margin: 0; padding: 5px 5px; border-radius: 5px; width:50%; max-width: 900px; }
+        /*.bottomright { position: fixed; bottom: 8px; right: 16px; opacity: 0.5; }*/
+        #togglePassword { cursor: pointer; margin-left: -30px; }
+        #eye { width: 20px; height: 15px; }
+        .btn input { cursor: pointer; }
+    </style>
     <script>
         function showPassword() {
         var image = document.getElementById('eye');
